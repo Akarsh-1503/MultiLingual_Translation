ab6b97d7a47951ba46f4e54f2e6a80da20c961d3
Akarsh Srivastav <sriakarsh2003@gmail.com>
2023-07-23 00:53:26 +0530

    Translator Added

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import tkinter as tk
+from tkinter import ttk
+from tkinter import *
 import nltk
 from textblob import TextBlob
 from newspaper import Article
 import pyttsx3
+from googletrans import Translator, LANGUAGES
 
 
 
@@ -62,7 +65,7 @@ def summarize():
     title.config(state='disabled')
     author.config(state='disabled')
     publication.config(state='disabled')
-    summary.config(state='disabled')
+    # summary.config(state='disabled')
     sentiment.config(state='disabled')
 
     # print(f'Title: {article.title}')
@@ -87,9 +90,65 @@ def speech():
     text_speech.say(answer)
     text_speech.runAndWait()
 
+
+def change(text="type", src="english", dest="hindi"):
+    text1= text
+    src1= src
+    dest1= dest
+    trans= Translator()
+    trans1= trans.translate(text1, src1, dest1)
+    return trans1.text
+
+def data():
+    url= utext.get('1.0', 'end').strip()
+    # nltk.download('punkt')
+
+    # url = 'http:// timesofindia.indiatimes.com/world/china/chinese-expert-warns-of-troops-entering-kashmir/articleshow/59516912.cms'
+
+    article= Article(url)
+
+    article.download()
+    article.parse()
+
+    article.nlp()
+
+    s= combo_sor.get()
+    d= combo_dest.get()
+    msg= article.summary
+    # print("message")
+    # print(msg)
+    textGet= change(text=msg, src=d, dest=s)
+    # print("output")
+    # print(textGet)
+    summary.delete(1.0, END)
+    summary.insert(END, textGet)
+
+
+#     button_change= Button(frame, text="Translate", )
 root= tk.Tk()
 root.title("News Summarizer")
 root.geometry('1200x600')
+root.config(bg='#ADD8E6')
+
+# frame= Frame(root).pack(side=BOTTOM)
+# sor_txt= Text(frame, font=("Times New Roman", 20, "bold"), wrap=WORD)
+
+list_text= list(LANGUAGES.values())
+
+sor_txt= Text(root)
+sor_txt.place(x=0,y=550, height=30, width=150)
+combo_sor= ttk.Combobox(root, value=list_text)
+combo_sor.place(x=300,y=550, height=30, width=150)
+combo_sor.set("english")
+
+button_change= Button(root, text="Translate", relief=RAISED, command= data)
+button_change.place(x=530,y=550, height=30, width=150)
+
+dest_txt= Text(root)
+dest_txt.place(x=900,y=550, height=30, width=150)
+combo_dest= ttk.Combobox(root, value=list_text)
+combo_dest.place(x=740,y=550, height=30, width=150)
+combo_dest.set("english")
 
 tlabel = tk.Label(root, text="Title")
 tlabel.pack()
@@ -121,8 +180,8 @@ publication.pack()
 slabel = tk.Label(root, text="Summary")
 slabel.pack()
 
-summary = tk.Text(root, height=20, width=140)
-summary.config(state='disabled', bg='#dddddd')
+summary = tk.Text(root, height=16, width=140)
+summary.config(state='normal', bg='#dddddd')
 summary.pack()
 
 
@@ -144,7 +203,11 @@ btn = tk.Button(root, text="Summarize", command= summarize)
 btn.pack()
 
 
-btn = tk.Button(root, text="Speech", command= speech)
+btn = tk.Button(root, text="Speak", command= speech)
 btn.pack()
+
+# btn = tk.Button(root, text="Translate", command= translator)
+# btn.pack()
+
 
 root.mainloop()
