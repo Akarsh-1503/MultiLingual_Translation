3b15309f20b492db77fb38099692fb130d65d01c
Akarsh Srivastav <sriakarsh2003@gmail.com>
2023-07-23 01:57:03 +0530

    Translator Added

--- main.py
+++ main.py
@@ -6,7 +6,57 @@ from textblob import TextBlob
 from newspaper import Article
 import pyttsx3
 from googletrans import Translator, LANGUAGES
+from playsound import playsound
+from gtts import gTTS 
+import os
 
+dic = ('afrikaans', 'af', 'albanian', 'sq', 
+       'amharic', 'am', 'arabic', 'ar',
+       'armenian', 'hy', 'azerbaijani', 'az', 
+       'basque', 'eu', 'belarusian', 'be',
+       'bengali', 'bn', 'bosnian', 'bs', 'bulgarian',
+       'bg', 'catalan', 'ca', 'cebuano',
+       'ceb', 'chichewa', 'ny', 'chinese (simplified)',
+       'zh-cn', 'chinese (traditional)',
+       'zh-tw', 'corsican', 'co', 'croatian', 'hr',
+       'czech', 'cs', 'danish', 'da', 'dutch',
+       'nl', 'english', 'en', 'esperanto', 'eo', 
+       'estonian', 'et', 'filipino', 'tl', 'finnish',
+       'fi', 'french', 'fr', 'frisian', 'fy', 'galician',
+       'gl', 'georgian', 'ka', 'german',
+       'de', 'greek', 'el', 'gujarati', 'gu',
+       'haitian creole', 'ht', 'hausa', 'ha',
+       'hawaiian', 'haw', 'hebrew', 'he', 'hindi',
+       'hi', 'hmong', 'hmn', 'hungarian',
+       'hu', 'icelandic', 'is', 'igbo', 'ig', 'indonesian', 
+       'id', 'irish', 'ga', 'italian',
+       'it', 'japanese', 'ja', 'javanese', 'jw',
+       'kannada', 'kn', 'kazakh', 'kk', 'khmer',
+       'km', 'korean', 'ko', 'kurdish (kurmanji)', 
+       'ku', 'kyrgyz', 'ky', 'lao', 'lo',
+       'latin', 'la', 'latvian', 'lv', 'lithuanian',
+       'lt', 'luxembourgish', 'lb',
+       'macedonian', 'mk', 'malagasy', 'mg', 'malay',
+       'ms', 'malayalam', 'ml', 'maltese',
+       'mt', 'maori', 'mi', 'marathi', 'mr', 'mongolian',
+       'mn', 'myanmar (burmese)', 'my',
+       'nepali', 'ne', 'norwegian', 'no', 'odia', 'or',
+       'pashto', 'ps', 'persian', 'fa',
+       'polish', 'pl', 'portuguese', 'pt', 'punjabi', 
+       'pa', 'romanian', 'ro', 'russian',
+       'ru', 'samoan', 'sm', 'scots gaelic', 'gd',
+       'serbian', 'sr', 'sesotho', 'st',
+       'shona', 'sn', 'sindhi', 'sd', 'sinhala', 'si',
+       'slovak', 'sk', 'slovenian', 'sl',
+       'somali', 'so', 'spanish', 'es', 'sundanese',
+       'su', 'swahili', 'sw', 'swedish',
+       'sv', 'tajik', 'tg', 'tamil', 'ta', 'telugu',
+       'te', 'thai', 'th', 'turkish',
+       'tr', 'ukrainian', 'uk', 'urdu', 'ur', 'uyghur',
+       'ug', 'uzbek',  'uz',
+       'vietnamese', 'vi', 'welsh', 'cy', 'xhosa', 'xh',
+       'yiddish', 'yi', 'yoruba',
+       'yo', 'zulu', 'zu')
 
 
 def summarize():
@@ -86,9 +136,29 @@ def speech():
 
     article.nlp()
     text_speech= pyttsx3.init()
-    answer= article.summary
-    text_speech.say(answer)
-    text_speech.runAndWait()
+    s= combo_sor.get()
+    d= combo_dest.get()
+    msg= article.summary
+    to_lang=d
+    to_lang = to_lang.lower()
+    to_lang = dic[dic.index(to_lang)+1]
+    textGet= change(text=msg, src=d, dest=s)
+    print("message")
+    print(textGet)
+    print(to_lang)
+    speak = gTTS(text=textGet, lang=to_lang, slow=False)
+    print("speak")
+    speak.save('captured_voice.mp3')
+  
+    # # Using OS module to run the translated voice
+    print("speaking")
+    playsound('captured_voice.mp3')
+    print("trying")
+    os.remove('captured_voice.mp3')
+    print("removing")
+    # # print(textGet)
+    # text_speech.say(textGet)
+    # text_speech.runAndWait()
 
 
 def change(text="type", src="english", dest="hindi"):
