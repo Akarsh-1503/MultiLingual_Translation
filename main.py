43888582ff9ee6791d12c8ede7923d866d62f1e3
Akarsh Srivastav <sriakarsh2003@gmail.com>
2023-07-23 03:43:28 +0530

    Finalized CSS

--- main.py
+++ main.py
@@ -62,9 +62,6 @@ dic = ('afrikaans', 'af', 'albanian', 'sq',
 def summarize():
     
     url= utext.get('1.0', "end").strip()
-    # nltk.download('punkt')
-
-    # url = 'http:// timesofindia.indiatimes.com/world/china/chinese-expert-warns-of-troops-entering-kashmir/articleshow/59516912.cms'
 
     article= Article(url)
 
@@ -75,7 +72,7 @@ def summarize():
 
     title.config(state='normal')
     author.config(state='normal')
-    publication.config(state='normal')
+    
     summary.config(state='normal')
     sentiment.config(state='normal')
 
@@ -86,23 +83,10 @@ def summarize():
     author.delete('1.0', 'end')
     if article.authors != [] :
         author.insert('1.0', article.authors)
-        # print("check")
         print(article.authors)
     else:
         author.insert('1.0', "Not Available")
         print(article.authors)
-    # author.insert('1.0', article.authors)
-
-    
-    publication.delete('1.0', 'end')
-    if article.publish_date != None :
-        publication.insert('1.0', article.publish_date)
-        # print("check")
-        # print(article.publish_date)
-    else:
-        publication.insert('1.0', "Not Available")
-        
-
     
     summary.delete('1.0', 'end')
     summary.insert('1.0', article.summary)
@@ -110,25 +94,14 @@ def summarize():
     analysis= TextBlob(article.text)
     sentiment.delete('1.0', 'end')
     sentiment.insert('1.0', f'Polarity: {analysis.polarity}, Sentiment: {"positive" if analysis.polarity >0 else "negative" if analysis.polarity<0 else "neutral"}')
-    # print(f'Sentiment: {"positive" if analysis.polarity >0 else "negative" if analysis.polarity<0 else "neutral"}')
 
     title.config(state='disabled')
     author.config(state='disabled')
-    publication.config(state='disabled')
-    # summary.config(state='disabled')
     sentiment.config(state='disabled')
-
-    # print(f'Title: {article.title}')
-    # print(f'Authors: {article.authors}')
-    # print(f'Summary: {article.summary}')
 
 
 def speech():
     url= utext.get('1.0', "end").strip()
-    # nltk.download('punkt')
-
-    # url = 'http:// timesofindia.indiatimes.com/world/china/chinese-expert-warns-of-troops-entering-kashmir/articleshow/59516912.cms'
-
     article= Article(url)
 
     article.download()
@@ -143,23 +116,10 @@ def speech():
     to_lang = to_lang.lower()
     to_lang = dic[dic.index(to_lang)+1]
     textGet= change(text=msg, src=d, dest=s)
-    print("message")
-    print(textGet)
-    print(to_lang)
     speak = gTTS(text=textGet, lang=to_lang, slow=False)
-    print("speak")
     speak.save('captured_voice.mp3')
-  
-    # # Using OS module to run the translated voice
-    print("speaking")
     playsound('captured_voice.mp3')
-    print("trying")
     os.remove('captured_voice.mp3')
-    print("removing")
-    # # print(textGet)
-    # text_speech.say(textGet)
-    # text_speech.runAndWait()
-
 
 def change(text="type", src="english", dest="hindi"):
     text1= text
@@ -171,10 +131,6 @@ def change(text="type", src="english", dest="hindi"):
 
 def data():
     url= utext.get('1.0', 'end').strip()
-    # nltk.download('punkt')
-
-    # url = 'http:// timesofindia.indiatimes.com/world/china/chinese-expert-warns-of-troops-entering-kashmir/articleshow/59516912.cms'
-
     article= Article(url)
 
     article.download()
@@ -185,42 +141,34 @@ def data():
     s= combo_sor.get()
     d= combo_dest.get()
     msg= article.summary
-    # print("message")
-    # print(msg)
     textGet= change(text=msg, src=d, dest=s)
-    # print("output")
-    # print(textGet)
     summary.delete(1.0, END)
     summary.insert(END, textGet)
 
 
-#     button_change= Button(frame, text="Translate", )
 root= tk.Tk()
-root.title("News Summarizer")
+root.title("MultiLingual Translation")
 root.geometry('1200x600')
 root.config(bg='#ADD8E6')
 
-# frame= Frame(root).pack(side=BOTTOM)
-# sor_txt= Text(frame, font=("Times New Roman", 20, "bold"), wrap=WORD)
-
 list_text= list(LANGUAGES.values())
 
-sor_txt= Text(root)
-sor_txt.place(x=0,y=550, height=30, width=150)
-combo_sor= ttk.Combobox(root, value=list_text)
-combo_sor.place(x=300,y=550, height=30, width=150)
+sor_txt= Text(root,  font=("Algerian", 12),bg="#ADD8E6")
+sor_txt.place(x=0,y=550, height=0, width=0)
+combo_sor= ttk.Combobox(root, value=list_text,  font=("Algerian", 12),justify="center")
+combo_sor.place(x=320,y=550, height=30, width=150)
 combo_sor.set("english")
 
-button_change= Button(root, text="Translate", relief=RAISED, command= data)
+button_change= Button(root, text="Translate", relief=RAISED, command= data, font=("Algerian", 12),justify="center")
 button_change.place(x=530,y=550, height=30, width=150)
 
-dest_txt= Text(root)
-dest_txt.place(x=900,y=550, height=30, width=150)
-combo_dest= ttk.Combobox(root, value=list_text)
+dest_txt= Text(root,  font=("Algerian", 12), bg="#ADD8E6")
+dest_txt.place(x=900,y=550, height=0, width=0)
+combo_dest= ttk.Combobox(root, value=list_text,  font=("Algerian", 12),justify="center")
 combo_dest.place(x=740,y=550, height=30, width=150)
 combo_dest.set("english")
 
-tlabel = tk.Label(root, text="Title")
+tlabel = tk.Label(root, text="Title", font=("Algerian", 12),bg='#ADD8E6')
 tlabel.pack()
 
 title = tk.Text(root, height=1, width=140)
@@ -229,7 +177,7 @@ title.pack()
 
 
 
-alabel = tk.Label(root, text="Author")
+alabel = tk.Label(root, text="Author", font=("Algerian", 12),bg='#ADD8E6')
 alabel.pack()
 
 author = tk.Text(root, height=1, width=140)
@@ -238,16 +186,7 @@ author.pack()
 
 
 
-plabel = tk.Label(root, text="Publication")
-plabel.pack()
-
-publication = tk.Text(root, height=1, width=140)
-publication.config(state='disabled', bg='#dddddd')
-publication.pack()
-
-
-
-slabel = tk.Label(root, text="Summary")
+slabel = tk.Label(root, text="Summary", font=("Algerian", 12),bg='#ADD8E6')
 slabel.pack()
 
 summary = tk.Text(root, height=16, width=140)
@@ -255,7 +194,7 @@ summary.config(state='normal', bg='#dddddd')
 summary.pack()
 
 
-selabel = tk.Label(root, text="Sentiment")
+selabel = tk.Label(root, text="Sentiment", font=("Algerian", 12),bg='#ADD8E6')
 selabel.pack()
 
 sentiment = tk.Text(root, height=1, width=140)
@@ -263,21 +202,18 @@ sentiment.config(state='disabled', bg='#dddddd')
 sentiment.pack()
 
 
-ulabel = tk.Label(root, text="URL")
+ulabel = tk.Label(root, text="URL", font=("Algerian", 12),bg='#ADD8E6')
 ulabel.pack()
 
 utext = tk.Text(root, height=1, width=140)
 utext.pack()
 
-btn = tk.Button(root, text="Summarize", command= summarize)
+btn = tk.Button(root, text="Summarize", command= summarize, font=("Times New Roman", 14, "italic"),bg='#ADD8E6')
 btn.pack()
 
 
-btn = tk.Button(root, text="Speak", command= speech)
+btn = tk.Button(root, text="Speak", command= speech,font=("Times New Roman", 11, "italic"),bg='#ADD8E6')
 btn.pack()
-
-# btn = tk.Button(root, text="Translate", command= translator)
-# btn.pack()
 
 
 root.mainloop()
